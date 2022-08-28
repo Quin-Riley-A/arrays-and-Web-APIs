b8b67fee3771751b733782e0d3a0401014d4375c
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-28 18:07:39 -0400

    add additional method for parsing form to array co-authored-by: Rafael Petrachini ​<raffp@me.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,23 @@ window.addEventListener("load", loadStuff);
 
 function parseData(event) {
   event.preventDefault();
-  let prompt = "prompt";
-  let n = 1;
-  let newStr;
-  let newArr = [];
-  let arrElem;
 
-  while (n < 4) {
-    newStr = prompt + n;
-    newArr.push(document.getElementById(newStr)).value;
-    console.log(newArr);
-    n++;
-  }
+//   let prompt = "prompt";
+//   let n = 1;
+//   let newStr;
+//   let newArr = [];
 
-  //
-  let prompt1 = document.getElementById(prompt1).value;
-  let prompt2 = document.getElementById(prompt2).value;
-  let prompt3 = document.getElementById(prompt3).value;
+//   while (n < 4) {
+//     newStr = prompt + n; // === "prompt" + 1 === "prompt1
+//     newArr.push((document.getElementById(newStr)).value);
+// //    console.log(newArr);
+//     n++;
+//   }
+  
+  let prompt1 = document.getElementById("prompt1").value;
+  let prompt2 = document.getElementById("prompt2").value;
+  let prompt3 = document.getElementById("prompt3").value;
 
   let valArr = [prompt1, prompt2, prompt3];
+  console.log("valArr is: " + valArr);
 }
